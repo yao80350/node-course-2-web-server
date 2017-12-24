fdedd1ae2322078672dd0f3a8c059dfdbe958d33
Yao <151736726@qq.com>
2017-12-24 10:55:36 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('unable to append file');
+		}
+	});
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('message.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		welcomeMessage: 'welcome to the website',
+		pageTitle: 'Home Page'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({errerMessage: "unable to fulfill your request"});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
